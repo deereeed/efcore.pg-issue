e5e5df1349c0e76b632b2aeade416a44ccfb1c74
mkopytin <mkopytin@custis.ru>
2020-11-23 16:43:06 +0300

    init

---
+++ Migrations/20201123131914_AddColumnWithConstraint2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFGetStarted.Migrations
+{
+    public partial class AddColumnWithConstraint2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateCheckConstraint(
+                name: "prop_ctx",
+                table: "Blogs",
+                sql: "new_property IN ('1', '2')");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NewProperty",
+                table: "Blogs",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "prop_ctx",
+                table: "Blogs");
+
+            migrationBuilder.DropColumn(
+                name: "NewProperty",
+                table: "Blogs");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace EFGetStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public class BloggingContext : DbContext
+        {
+            public DbSet<Blog> Blogs { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder options)
+                => options.UseNpgsql("Host=127.0.0.1; Database=my_db;Username=my_user;Password=my_pwd");
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+            }
+        }
+
+        public class Blog
+        {
+            public int BlogId { get; set; }
+            public string NewProperty { get; set; }
+        }
+
+        public class BlogConfiguration : IEntityTypeConfiguration<Blog>
+        {
+            public void Configure(EntityTypeBuilder<Blog> builder)
+            {
+                builder.HasKey(x => x.BlogId);
+                builder.Property(x => x.NewProperty).HasMaxLength(100); 
+                builder.HasCheckConstraint("prop_ctx", "new_property IN ('1', '2')");
+            }
+        }
+    }
+}
